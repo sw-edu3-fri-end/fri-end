419d041250f802db328df76d619abc29bfea2908
ssoso27 <sohi8290@gmail.com>
2019-06-01 03:45:57 +0900

    [Coaching] 훈수 보기 기능 추가

--- Submission/views.py
+++ Submission/views.py
@@ -5,7 +5,11 @@ from .models import Submission
 
 def detail(request, pk):
     submission =  get_object_or_404(Submission, pk=pk)
-    return render(request, 'Submission/detail.html', {'submission' : submission})
+    content = {
+        'submission' : submission,
+        'coachings' : submission.coaching_set.all()
+    }
+    return render(request, 'Submission/detail.html', content)
 
 def success(request, pk):
     # TODO : 해당 로직 실행자가 해당 과제 등록자인지 확인하는 로직 필요
