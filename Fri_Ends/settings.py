b070f74dab90794fb66902ff92f4c983d9a8921d
yungikim <khyerim@naver.com>
2019-06-01 01:39:40 +0900

    과제 상세페이지 개발중

--- Fri_Ends/settings.py
+++ Fri_Ends/settings.py
@@ -126,5 +126,5 @@ STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static'),
 ]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/static/img/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/img/media')
