75cf35b0bba2735ac795f7fdb840ab97dcce11d6
ssoso27 <sohi8290@gmail.com>
2019-06-01 03:25:07 +0900

    [Coaching] Coaching 모델 생성
    - 코칭 모델, 코칭 어드민 생성

--- Submission/admin.py
+++ Submission/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
-from .models import Submission
+from .models import Submission, Coaching
 
 class SubmissionAdmin(admin.ModelAdmin):
     fields = ['title', 'image', 'content', 'status', 'created_at', 'updated_at']
 
+class CoachingAdmin(admin.ModelAdmin):
+    fields = ['content', 'submission', 'created_at', 'updated_at']
+
 admin.site.register(Submission, SubmissionAdmin)
+admin.site.register(Coaching, CoachingAdmin)
