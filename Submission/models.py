3de67f75584bd8adba21abaeda6784d6a590ef73
ssoso27 <sohi8290@gmail.com>
2019-05-31 22:49:21 +0900

    [Submission] Submission 모델 생성
    - 추후 writer 추가 요망

--- Submission/models.py
+++ Submission/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+import Assignment
+
+class Submission(models.Model):
+    title = models.CharField(max_length=20)
+    content = models.TextField()
+    # writer = models.ForeignKey(User, on_delete=models.CASCADE)
+    assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'[{self.pk}] {self.title}'
