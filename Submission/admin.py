9928fcfb5559c60de41ca65bd2b4e29edb986432
ssoso27 <sohi8290@gmail.com>
2019-05-31 23:21:39 +0900

    [Submission] 모델 수정 - image 추가

--- Submission/admin.py
+++ Submission/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from .models import Submission
 
 class SubmissionAdmin(admin.ModelAdmin):
-    fields = ['title', 'content', 'status', 'created_at', 'updated_at']
+    fields = ['title', 'image', 'content', 'status', 'created_at', 'updated_at']
 
 admin.site.register(Submission, SubmissionAdmin)
--- Submission/models.py
+++ Submission/models.py
@@ -3,11 +3,15 @@ from django.db import models
 # Create your models here.
 import Assignment
 
+def submission_image_path(instance, filename):
+    return f'submission/{filename}'
+
 class Submission(models.Model):
     title = models.CharField(max_length=20)
     content = models.TextField()
     # writer = models.ForeignKey(User, on_delete=models.CASCADE)
     # assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to = submission_image_path, null=True)
     status = models.CharField(max_length=20)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
