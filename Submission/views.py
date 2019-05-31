4987db5a1184618066291f6700112e69f463d7fd
ssoso27 <sohi8290@gmail.com>
2019-06-01 04:03:31 +0900

    [Coaching] 훈수 두기 기능 추가

--- Submission/views.py
+++ Submission/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 
-from .models import Submission
+from .models import Submission, Coaching
 
 def detail(request, pk):
     submission =  get_object_or_404(Submission, pk=pk)
@@ -18,7 +18,7 @@ def success(request, pk):
         # TODO : 해당 제출자의 point up 로직 필요
         submission.status = 'succeed'
         submission.save()
-    return redirect('Submission:detail pk')
+    return redirect('Submission:detail', pk)
 
 def fail(request, pk):
     # TODO : 해당 로직 실행자가 해당 과제 등록자인지 확인하는 로직 필요
@@ -26,4 +26,12 @@ def fail(request, pk):
     if request.method == 'POST':
         submission.status = 'failed'
         submission.save()
-    return redirect('Submission:detail pk')
+    return redirect('Submission:detail', pk)
+
+def create_coaching(request, submission_pk):
+    submission = get_object_or_404(Submission, pk=submission_pk)
+    if request.method == 'POST':
+        content = request.POST.get('content')
+        coaching = Coaching(content=content, submission=submission)
+        coaching.save()
+    return redirect('Submission:detail', submission_pk)
